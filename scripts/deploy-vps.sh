79a1f72efd3d059a238be1e4807cd17f63da3a43
Timur Gromov <ruslanmamedov@MacBook-Air-Ruslan.local>
2026-03-20 02:18:03 +0300

    Restart frontend during VPS deploy

--- scripts/deploy-vps.sh
+++ scripts/deploy-vps.sh
@@ -2,4 +2,4 @@
 set -eu
 
 ssh -o IdentitiesOnly=yes -i ~/.ssh/cloudru.key user1@185.50.203.2 \
-  'cd /home/user1/EventBudjet && git pull && sudo docker compose up -d --build'
+  'cd /home/user1/EventBudjet && git pull && sudo docker compose up -d --build && sudo docker compose restart frontend nginx'
